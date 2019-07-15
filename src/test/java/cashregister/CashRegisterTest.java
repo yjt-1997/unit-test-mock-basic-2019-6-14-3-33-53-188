26391507a0ebe7cf07eac1c66ad431a47140e67e
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-15 16:57:42 +0800

    commit pass

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,32 +1,53 @@
 package cashregister;
 
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        Printer printer = new Printer();
+        Item[] items = new Item[]{new Item("item1", 10.0)};
+        Purchase purchase = new Purchase(items);
+        CashRegister cashRegister = new CashRegister(printer);
+
+        assertThrows(UnsupportedOperationException.class, () -> printer.print(purchase.asString()));
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        CashRegister cashRegister = new CashRegister(new Printer());
+        Purchase purchase = mock(Purchase.class);
+
+        when(purchase.asString()).thenReturn("Some Items infos");
+        cashRegister.process(purchase);
+
+        assertEquals(systemOut(), "Some Items infos");
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
-        //given
-        //when
-        //then
+
     }
 
 }
